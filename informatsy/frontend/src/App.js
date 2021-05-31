06c0bf9df444391e7943cfc3548670916e77bfd2
pranav-bharadwaj <pranavbharadwaj2001@gmail.com>
2021-06-01 00:47:32 +0530

    popup noti

--- informatsy/frontend/src/App.js
+++ informatsy/frontend/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Developing Informatsy from React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import PopupAccount from "./components/PopupAccount";
+import Home from "./components/Home";
+export default class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/login" component={Login} />
+            <Route path="/popup" component={PopupAccount} />
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
-
-export default App;
