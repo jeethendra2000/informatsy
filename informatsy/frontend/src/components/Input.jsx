03d39c2f760f35796af1de14afc083ec25084333
pranav-bharadwaj <pranavbharadwaj2001@gmail.com>
2021-06-05 01:22:23 +0530

    loginpage

--- informatsy/frontend/src/components/Input.jsx
+++ informatsy/frontend/src/components/Input.jsx
@@ -11,42 +11,13 @@ export class Input extends Component {
     const img = this.props.component;
     const classname = this.props.classname;
     return (
-      <div className={classname}>
-        <fieldset className="input_main">
-          {img}
-          <div
-            className="input_lb"
-            onClick={() => {
-              var ele = document.querySelector("." + classname + " .lab");
-              var main = document.querySelector(
-                "." + classname + " .input_main"
-              );
-              var input = document.querySelector(
-                "." + classname + " .input_cls"
-              );
-              input.focus();
-              main.style.transition = "0.2s ease-in-out";
-              var pos = 25;
-              var font = 14;
-              var id = setInterval(() => {
-                if (pos <= -21) {
-                  clearInterval(id);
-                } else {
-                  pos = pos - 5;
-                  font = font - 0.2;
-                  ele.style.top = pos + "%";
-                  ele.style.fontSize = font + "px";
-                }
-              }, 10);
-
-              ele.style.backgroundColor = "white";
-              main.style.border = "2px blue solid";
-            }}
-          >
-            <input
-              type={type}
-              className="input_cls"
-              onBlur={() => {
+      <div className="input_component_main">
+        <div className={classname}>
+          <fieldset className="input_main">
+            {img}
+            <div
+              className="input_lb"
+              onClick={() => {
                 var ele = document.querySelector("." + classname + " .lab");
                 var main = document.querySelector(
                   "." + classname + " .input_main"
@@ -54,28 +25,59 @@ export class Input extends Component {
                 var input = document.querySelector(
                   "." + classname + " .input_cls"
                 );
-                if (input.value === "") {
-                  var pos = -21;
-                  var font = 12;
-                  var id = setInterval(() => {
-                    if (pos >= 20) {
-                      clearInterval(id);
-                    } else {
-                      pos = pos + 5;
-                      font = font + 0.2;
-                      ele.style.top = pos + "%";
-                      ele.style.fontSize = font + "px";
-                    }
-                  }, 10);
-                  ele.style.backgroundColor = "inherit";
-                  main.style.border = "2px solid #f0f0f0";
-                }
-              }}
-            />
+                input.focus();
+                main.style.transition = "0.2s ease-in-out";
+                var pos = 25;
+                var font = 14;
+                var id = setInterval(() => {
+                  if (pos <= -21) {
+                    clearInterval(id);
+                  } else {
+                    pos = pos - 5;
+                    font = font - 0.2;
+                    ele.style.top = pos + "%";
+                    ele.style.fontSize = font + "px";
+                  }
+                }, 10);
 
-            <legend className="lab">{name}</legend>
-          </div>
-        </fieldset>
+                ele.style.backgroundColor = "white";
+                main.style.border = "2px blue solid";
+              }}
+            >
+              <input
+                type={type}
+                className="input_cls"
+                onBlur={() => {
+                  var ele = document.querySelector("." + classname + " .lab");
+                  var main = document.querySelector(
+                    "." + classname + " .input_main"
+                  );
+                  var input = document.querySelector(
+                    "." + classname + " .input_cls"
+                  );
+                  if (input.value === "") {
+                    var pos = -21;
+                    var font = 12;
+                    var id = setInterval(() => {
+                      if (pos >= 20) {
+                        clearInterval(id);
+                      } else {
+                        pos = pos + 5;
+                        font = font + 0.2;
+                        ele.style.top = pos + "%";
+                        ele.style.fontSize = font + "px";
+                      }
+                    }, 10);
+                    ele.style.backgroundColor = "inherit";
+                    main.style.border = "2px solid #f0f0f0";
+                  }
+                }}
+              />
+
+              <legend className="lab">{name}</legend>
+            </div>
+          </fieldset>
+        </div>
       </div>
     );
   }
--- informatsy/frontend/src/components/Login.jsx
+++ informatsy/frontend/src/components/Login.jsx
@@ -31,94 +31,103 @@ export class Login extends Component {
     return (
       <div className="login_main">
         <div className="login_parent">
-          <div className="head_container">
-            <div>
-              <img
-                src={logo}
-                className="top_logo"
-                width={img_size - 3}
-                height={img_size - 3}
-              />
-              <span>Informatsy</span>
+          <div className="login_side_bar">
+            <div className="login_side_p1"></div>
+
+            <div className="login_side_p2"></div>
+            <div className="login_side_p3"></div>
+            <div className="login_side_p4"></div>
+          </div>
+          <div className="login_child">
+            <div className="head_container">
+              <div>
+                <img
+                  src={logo}
+                  className="top_logo"
+                  width={img_size - 3}
+                  height={img_size - 3}
+                />
+                <span>Informatsy</span>
+              </div>
             </div>
-          </div>
-          <div className="top_tag">
-            <h2>Welcome!</h2>
-            <p>Sign in to continue</p>
-          </div>
-          <div className="form_main">
-            <div className="forms_content1">
-              <Input
-                name="Username or Email"
-                classname="one"
-                type="text"
-                component={<PersonRoundedIcon style={style} />}
-              />
+            <div className="top_tag">
+              <h2>Welcome!</h2>
+              <p>Sign in to continue</p>
             </div>
-            <div className="forms_content1">
-              <Input
-                name="Password"
-                classname="two"
-                type="password"
-                component={<LockOpenRoundedIcon style={style} />}
-              />
+            <div className="form_main">
+              <div className="forms_content1">
+                <Input
+                  name="Username or Email"
+                  classname="one"
+                  type="text"
+                  component={<PersonRoundedIcon style={style} />}
+                />
+              </div>
+              <div className="forms_content1">
+                <Input
+                  name="Password"
+                  classname="two"
+                  type="password"
+                  component={<LockOpenRoundedIcon style={style} />}
+                />
+              </div>
             </div>
-          </div>
-          <div className="button_main">
-            <Button
-              className="btn_1"
-              id="btn"
-              variant="contained"
-              color="primary"
-              component={Link}
-              to="/login"
-              disableElevation
-            >
-              Login
-            </Button>
-          </div>
-          <div className="Bottom_main">
-            <p>or via social media</p>
-            <div className="social_acc">
-              <IconButton
-                aria-label="facebook"
-                className="btn_sa"
+            <div className="button_main">
+              <Button
+                className="btn_1"
+                id="btn"
+                variant="contained"
+                color="primary"
                 component={Link}
                 to="/login"
+                disableElevation
               >
-                <img
-                  src={fb_icon}
-                  alt="facebook"
-                  width={img_size}
-                  height={img_size}
-                />
-              </IconButton>
-              <IconButton
-                className="btn_sa"
-                aria-label="google"
-                component={Link}
-                to="/login"
-              >
-                <img
-                  src={g_icon}
-                  alt="google"
-                  width={img_size}
-                  height={img_size}
-                />
-              </IconButton>
-              <IconButton
-                aria-label="linkedIn"
-                className="btn_sa"
-                component={Link}
-                to="/login"
-              >
-                <img
-                  src={li_icon}
-                  alt="linkedIn"
-                  width={img_size + 3}
-                  height={img_size + 3}
-                />
-              </IconButton>
+                Login
+              </Button>
+            </div>
+            <div className="Bottom_main">
+              <p>or via social media</p>
+              <div className="social_acc">
+                <IconButton
+                  aria-label="facebook"
+                  className="btn_sa"
+                  component={Link}
+                  to="/login"
+                >
+                  <img
+                    src={fb_icon}
+                    alt="facebook"
+                    width={img_size}
+                    height={img_size}
+                  />
+                </IconButton>
+                <IconButton
+                  className="btn_sa"
+                  aria-label="google"
+                  component={Link}
+                  to="/login"
+                >
+                  <img
+                    src={g_icon}
+                    alt="google"
+                    width={img_size}
+                    height={img_size}
+                  />
+                </IconButton>
+                <IconButton
+                  aria-label="linkedIn"
+                  className="btn_sa"
+                  component={Link}
+                  to="/login"
+                >
+                  <img
+                    src={li_icon}
+                    alt="linkedIn"
+                    width={img_size + 3}
+                    height={img_size + 3}
+                  />
+                </IconButton>
+              </div>
             </div>
           </div>
         </div>
